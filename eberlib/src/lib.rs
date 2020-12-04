f6b1713d1b2f5152a77876e4f95b4236abdc3c58
Jonathan Queiroz <dev@johnjq.com>
2020-12-03 19:19:42 -0800

    Implement letter-decomposition of words

--- eberlib/src/lib.rs
+++ eberlib/src/lib.rs
@@ -39,6 +39,230 @@ pub mod presentation {
     }
 }
 
+pub mod utils {
+    use super::dict::Entry;
+
+    pub mod letter_decomposition {
+        pub enum LetterType {
+            VowelTail,
+            Consonant,
+        }
+
+        /// A pattern of consonants and vowel tails, e.g. CVC or CCVC
+        type LetterPattern = Vec<LetterType>;
+
+        pub static VOWELS : [char; 6] = [ 'a', 'e', 'i', 'o', 'u', 'y' ];
+
+        pub static CODA : [char; 3] = [ 'l', 'n', 'r' ];
+
+        pub static SEPARATORS : [char; 4] = [ 'l', 'n', 'r', 'h' ];
+
+        pub static CONSONANTS : [char; 17] = [ 'b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'x', 'z' ];
+
+        pub fn is_valid_letter(letter: &char) -> bool {
+            return is_vowel(&letter) || is_consonant(&letter) || is_h(&letter);
+        }
+
+        pub fn is_vowel(letter: &char) -> bool {
+            return VOWELS.contains(letter);
+        }
+
+        pub fn is_separator(letter: &char) -> bool {
+            return SEPARATORS.contains(letter);
+        }
+
+        pub fn is_coda(letter: &char) -> bool {
+            return CODA.contains(letter);
+        }
+
+        pub fn is_h(letter: &char) -> bool {
+            return letter == &'h';
+        }
+
+        pub fn is_consonant(letter: &char) -> bool {
+            return CONSONANTS.contains(letter);
+        }
+
+        pub enum DecompositionError {
+            EmptyWord,
+            AllConsonants,
+            UnexpectedSeparatorAtWordStart(char),
+            UnexpectedHAfterConsonant(usize),
+            UnexpectedNonCodaConsonantAfterVowel(usize, char),
+            UnexpectedVowelAfterDiphthong(usize, char),
+            UnexpectedConsonantAfterSeparator(usize, char),
+            UnexpectedHAfterSeparator(usize, char),
+            InvalidWordEnding(char),
+            UnknownSymbol(usize, char),
+        }
+
+        enum DecompositionState {
+            Empty,     // has not seen anything yet (which means an immediately following coda is not allowed)
+            Consonant, // has seen only consonants so far (has the same transition rules as Empty, except that an immediately following coda is allowed)
+            Vowel1,    // has just seen a single vowel
+            Vowel2,    // has just seen two consecutive vowels (i.e. a diphthong)
+            Separator, // has just seen a separator
+        }
+
+        /// Decomposes a word into a pattern of consonants and vowel tails
+        pub fn decompose_word(word: &str) -> Result<LetterPattern, DecompositionError> {
+            // TODO: recognize more invalid cases, such as:
+            //   1. invalid diphthongs
+            //   2. invalid consonant pairs
+            //   3. more than two consecutive consonants?? (probably not necessary to validate here)
+            //  For (2) and (3), it should suffice to break "Consonant" into "Consonant1(char)" and "Consonant2"
+            let mut result = Vec::new();
+            let mut state = DecompositionState::Empty;
+            let mut last_letter : Option<char> = None;
+            for (position, letter) in word.chars().enumerate() {
+                last_letter = Some(letter);
+                match &state {
+                    DecompositionState::Empty => {
+                        // If we have not seen anything, then we must see either a vowel or a non-coda consonant
+                        if is_vowel(&letter) {
+                            state = DecompositionState::Vowel1;
+                            result.push(LetterType::VowelTail);
+                        } else if is_separator(&letter) {
+                            return Err(DecompositionError::UnexpectedSeparatorAtWordStart(letter))
+                        } else if is_consonant(&letter) {
+                            state = DecompositionState::Consonant;
+                            result.push(LetterType::Consonant);
+                        } else {
+                            return Err(DecompositionError::UnknownSymbol(position, letter));
+                        }
+                    }
+                    DecompositionState::Consonant => {
+                        if is_vowel(&letter) {
+                            state = DecompositionState::Vowel1;
+                            result.push(LetterType::VowelTail);
+                        } else if is_consonant(&letter) {
+                            state = DecompositionState::Consonant;
+                            result.push(LetterType::Consonant);
+                        } else if is_h(&letter) {
+                            return Err(DecompositionError::UnexpectedHAfterConsonant(position));
+                        } else {
+                            return Err(DecompositionError::UnknownSymbol(position, letter));
+                        }
+                    }
+                    DecompositionState::Vowel1 => {
+                        // The first vowel must be followed by either a second vowel or a separator
+                        if is_vowel(&letter) {
+                            state = DecompositionState::Vowel2;
+                        } else if is_separator(&letter) {
+                            state = DecompositionState::Separator;
+                        } else if is_consonant(&letter) {
+                            return Err(DecompositionError::UnexpectedNonCodaConsonantAfterVowel(position, letter));
+                        } else {
+                            return Err(DecompositionError::UnknownSymbol(position, letter));
+                        }
+                    }
+                    DecompositionState::Vowel2 => {
+                        // The second vowel must be followed by a separator
+                        if is_vowel(&letter) {
+                            return Err(DecompositionError::UnexpectedVowelAfterDiphthong(position, letter));
+                        } else if is_separator(&letter) {
+                            state = DecompositionState::Separator;
+                        } else if is_consonant(&letter) {
+                            return Err(DecompositionError::UnexpectedNonCodaConsonantAfterVowel(position, letter));
+                        } else {
+                            return Err(DecompositionError::UnknownSymbol(position, letter));
+                        }
+                    }
+                    DecompositionState::Separator => {
+                        // A separator must be followed by a vowel
+                        if is_vowel(&letter) {
+                            state = DecompositionState::Vowel1;
+                        } else if is_consonant(&letter) {
+                            return Err(DecompositionError::UnexpectedConsonantAfterSeparator(position, letter));
+                        } else if is_h(&letter) {
+                            return Err(DecompositionError::UnexpectedHAfterSeparator(position, letter));
+                        } else {
+                            return Err(DecompositionError::UnknownSymbol(position, letter));
+                        }
+                    }
+                }
+            }
+
+            // Validate the ending state
+            match state {
+                DecompositionState::Empty => {
+                    return Err(DecompositionError::EmptyWord);
+                }
+                DecompositionState::Consonant => {
+                    // A word cannot consist entirely of consonants
+                    return Err(DecompositionError::AllConsonants);
+                }
+                DecompositionState::Vowel1 | DecompositionState::Vowel2 => {
+                    // These are valid terminal states where nothing needs to be done
+                    return Ok(result);
+                }
+                DecompositionState::Separator => {
+                    // This is a valid terminal state, but the last character needs to be a coda (not h), and some postprocessing is needed to add it as a separate consonant (as it is not part of the vowel tail)
+                    if let Some(letter) = last_letter {
+                        if (is_coda(&letter)) {
+                            result.push(LetterType::Consonant);
+                            return Ok(result);
+                        } else {
+                            assert!(is_valid_letter(&letter));
+                            return Err(DecompositionError::InvalidWordEnding(letter));
+                        }
+                    } else {
+                        // This should never happen, but let's handle this case nevertheless
+                        return Err(DecompositionError::EmptyWord);
+                    }
+                }
+            }
+        }
+    }
+
+    pub enum RootType {
+        VC,
+        CVC,
+        CCV,
+        CCVC,
+    }
+
+    pub enum InvalidRootTypeError {
+        NotARoot,
+        InvalidLetterPattern,
+    }
+
+    pub fn is_root(entry: &Entry) -> bool {
+        return entry.family == "R";
+    }
+
+    pub fn get_root_type(entry: &Entry) -> Result<RootType, InvalidRootTypeError> {
+        if is_root(entry) {
+            Err(InvalidRootTypeError::NotARoot)
+        } else {
+            Ok(RootType::CVC)
+        }
+    }
+}
+
+pub mod validation {
+    use super::dict::Entry;
+    use super::utils;
+
+    pub struct ValidationResult {
+        key: String,
+        message: String,
+        suggestions: Vec<String>,
+    }
+
+    pub fn validate_entry(entry: &Entry) {
+        // General validations
+        // . -> '
+
+        // Root-specific validations
+        if utils::is_root(entry) {
+            match utils::get_root_type(entry) {
+                _ => {}
+            }
+        }
+    }
+}
+
 pub mod cli {
     use std::io::{self, BufRead};
     use super::{presentation,dict::Dictionary};
